085f5fab17ba2497fb764a51ede0a448e20b90e1
Thang Le <thanglq@inspirelab.io>
2022-01-23 20:44:16 +0700

    Exercise 3: Tweak main view

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { WEIGHTS } from '../../constants';
+import { QUERIES, WEIGHTS } from '../../constants';
 
 import Breadcrumbs from '../Breadcrumbs';
 import Select from '../Select';
@@ -15,28 +15,26 @@ const ShoeIndex = ({ sortId, setSortId }) => {
       <MainColumn>
         <Header>
           <Title>Running</Title>
-          <Select
-            label="Sort"
-            value={sortId}
-            onChange={(ev) => setSortId(ev.target.value)}
-          >
-            <option value="newest">Newest Releases</option>
-            <option value="price">Price</option>
-          </Select>
+          <SelectWrapper>
+            <Select label='Sort' value={sortId} onChange={(ev) => setSortId(ev.target.value)}>
+              <option value='newest'>Newest Releases</option>
+              <option value='price'>Price</option>
+            </Select>
+          </SelectWrapper>
         </Header>
         <Spacer size={32} />
         <ShoeGrid />
       </MainColumn>
       <LeftColumn>
         <Breadcrumbs>
-          <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale/shoes">
-            Shoes
-          </Breadcrumbs.Crumb>
+          <Breadcrumbs.Crumb href='/'>Home</Breadcrumbs.Crumb>
+          <Breadcrumbs.Crumb href='/sale'>Sale</Breadcrumbs.Crumb>
+          <Breadcrumbs.Crumb href='/sale/shoes'>Shoes</Breadcrumbs.Crumb>
         </Breadcrumbs>
-        <Spacer size={42} />
-        <ShoeSidebar />
+        <SidebarWrapper>
+          <Spacer size={42} />
+          <ShoeSidebar />
+        </SidebarWrapper>
       </LeftColumn>
     </Wrapper>
   );
@@ -47,10 +45,19 @@ const Wrapper = styled.div`
   flex-direction: row-reverse;
   align-items: baseline;
   gap: 32px;
+
+  @media (${QUERIES.tabletAndDown}) {
+    flex-direction: column-reverse;
+    gap: 8px;
+  }
 `;
 
 const LeftColumn = styled.div`
   flex-basis: 248px;
+
+  @media (${QUERIES.tabletAndDown}) {
+    flex-basis: auto;
+  }
 `;
 
 const MainColumn = styled.div`
@@ -66,6 +73,18 @@ const Header = styled.header`
 const Title = styled.h2`
   font-size: 1.5rem;
   font-weight: ${WEIGHTS.medium};
+`;
+
+const SelectWrapper = styled.div`
+  @media (${QUERIES.phoneAndDown}) {
+    display: none;
+  }
+`;
+
+const SidebarWrapper = styled.div`
+  @media (${QUERIES.tabletAndDown}) {
+    display: none;
+  }
 `;
 
 export default ShoeIndex;
